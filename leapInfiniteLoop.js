19b77a0f708f9dc55275c99dbe5417545e8133ca
morninghours01 <brycepotts16@gmail.com>
2020-09-13 23:29:14 -0400

    Del01 - Step 65 - working

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,11 +1,30 @@
 var controllerOptions = {};
 let x = window.innerWidth/2;
 let y = window.innerHeight/2;
+
+function handleFingers(fingers){
+  for (i=0; i<fingers.length; i++){
+    if(i===1) console.log(fingers[i].tipPosition);
+  }
+}
+
+function handleHand(hand){
+  handleFingers(hand.fingers)
+}
+
+function handleFrame(frame){
+  if(frame.hands.length === 1){
+    handleHand(frame.hands[0])
+  }
+}
+
+
 Leap.loop(controllerOptions, function(frame)
   {
-    clear();
-    circle(x, y, 50);
-    x+=Math.random()*2-1;
-    y+=Math.random()*2-1;
-  }
+    handleFrame(frame);
+    // clear();
+    // circle(x, y, 50);
+    // x+=Math.random()*2-1;
+    // y+=Math.random()*2-1;
+}
 );
