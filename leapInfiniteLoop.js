2ff9738d77e829b9b1ac1df5cde281428a24dc0e
morninghours01 <brycepotts16@gmail.com>
2020-09-14 00:12:38 -0400

    Del01-Step 69 - didnt figure it out with the console closed, trying to figure it out now

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -2,14 +2,35 @@ var controllerOptions = {};
 let x = window.innerWidth/2;
 let y = window.innerHeight/2;
 var z;
+let rawXMin = 3*window.innerWidth/4;
+let rawXMax = window.innerWidth/4;
+let rawYMin = 3*window.innerHeight/4;
+let rawYMax = window.innerHeight/4;
+
 
 function handleFingers(fingers){
   for (i=0; i<fingers.length; i++){
     if(i===1){
-      console.log(fingers[i].tipPosition);
+      //console.log(fingers[i].tipPosition);
       x = fingers[i].tipPosition[0]+window.innerWidth/2;
-      y = window.innerWidth-fingers[i].tipPosition[1];
+      y = window.innerWidth-fingers[i].tipPosition[1]
+      y = fingers[i].tipPosition[1];
       z = fingers[i].tipPosition[2];
+      //resetting max and mins
+      if(x < rawXMin){
+        rawXmin = x;
+      }
+      else if(x > rawXMax){
+        rawXMax = x
+      }
+
+      if(y < rawYMin){
+        rawYmin = y;
+      }
+      else if(y > rawYMax){
+        rawYMax = y
+      }
+      console.log([rawXMin,rawXMax,rawYMin,rawYMax])
     }
 
   }
