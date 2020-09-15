ab5a26be708d5a0a470483391be6839afa12724f
morninghours01 <brycepotts16@gmail.com>
2020-09-14 23:47:03 -0400

    Del02 - Completed Deliverable - Working

--- leapDrawLines.js
+++ leapDrawLines.js
@@ -41,8 +41,8 @@ function transformCoordinates(x,y){
 }
 
 function handleBone(bone,boneType){
-  console.log(bone);
-  console.log(boneType);
+  //console.log(bone);
+  //console.log(boneType);
   x1= bone.nextJoint[0];
   y1 = bone.nextJoint[1];
   z1 = bone.nextJoint[2];
@@ -52,19 +52,33 @@ function handleBone(bone,boneType){
   y2 = bone.prevJoint[1];
   z2 = bone.prevJoint[2];
   [x2,y2] = transformCoordinates(x2,y2);
+  strokeWeight(8-2*boneType);
+
+  stroke(255-256/5*(boneType+1))
   line(x1,y1,x2,y2);
 
 }
 
-function handleFinger(finger){
-    for(k=0;k<finger.bones.length;k++){
-      handleBone(finger.bones[k],finger.bones[k].type);
-    }
-}
+// function handleFinger(finger){
+//     for(k=0;k<finger.bones.length;k++){
+//       handleBone(finger.bones[k],finger.bones[k].type);
+//     }
+// }
 
+// function handleHand(hand){
+//   for (i=0; i<hand.fingers.length; i++){
+//     handleFinger(hand.fingers[i])
+//   }
+// }
 function handleHand(hand){
-  for (i=0; i<hand.fingers.length; i++){
-    handleFinger(hand.fingers[i])
+  let numFingerBones = hand.fingers[0].bones.length;
+  let numFingers = hand.fingers.length;
+
+  for(k=numFingerBones-1;k>=0;k--){
+    for (i=0; i<numFingers; i++){
+      handleBone(hand.fingers[i].bones[k],hand.fingers[i].bones[k].type);
+      //console.log([i,k])
+    }
   }
 }
 
