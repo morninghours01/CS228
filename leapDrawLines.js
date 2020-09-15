f26e07c1ed05152ed40c30767c07502bfe1a9a18
morninghours01 <brycepotts16@gmail.com>
2020-09-14 22:56:21 -0400

    Del02 - Step19 - working

--- leapDrawLines.js
+++ leapDrawLines.js
@@ -1,53 +1,71 @@
 var controllerOptions = {};
-let x;
-var y;
-var z;
+let x1;
+let x2;
+let y1;
+let y2;
+let z1;
+let z2;
+
 let rawXMin = 500;
 let rawXMax = -500;
 let rawYMin = 500;
 let rawYMax = -500;
 var rangeX;
 var rangeY;
-var percentX;
-var percentY
+var percentX1;
+var percentX2;
+var percentY1;
+var percentY2;
 
 function handleBone(bone){
   //console.log(bone);
-  x = bone.nextJoint[0];
-  y = bone.nextJoint[1];
-  z = bone.nextJoint[2];
+  x1= bone.nextJoint[0];
+  y1 = bone.nextJoint[1];
+  z1 = bone.nextJoint[2];
+
+  x2 = bone.prevJoint[0];
+  y2 = bone.prevJoint[1];
+  z2 = bone.prevJoint[2];
   //resetting max and mins
-  if(x < rawXMin){
-    rawXMin = x;
+  if(x2 < rawXMin){
+    rawXMin = x2;
     //console.log("xmin is true")
   }
-  if(x > rawXMax){
-    rawXMax = x;
+  if(x2 > rawXMax){
+    rawXMax = x2;
     //console.log("xmax is true")
   }
 
-  if(y < rawYMin){
-    rawYMin = y;
+  if(y2 < rawYMin){
+    rawYMin = y2;
     //console.log("ymin is true")
   }
-  if(y > rawYMax){
-    rawYMax = y;
+  if(y2 > rawYMax){
+    rawYMax = y2;
     //console.log("xmax is true")
   }
   //console.log(x,y)
   //console.log([rawXMin,rawXMax,rawYMin,rawYMax]);
   rangeX = rawXMax-rawXMin;
   rangeY = rawYMax-rawYMin;
-  percentX = (x-rawXMin)/rangeX
-  x = percentX * window.innerWidth
-  percentY = (y-rawYMin)/rangeY
-  y = (1-percentY) * window.innerWidth
+
+  percentX1 = (x1-rawXMin)/rangeX
+  x1 = percentX1 * window.innerWidth
+  percentX2 = (x2-rawXMin)/rangeX
+  x2 = percentX2 * window.innerWidth
+  //
+  // percentY1 = (y1-rawYMin)/rangeY
+  // y1 = (1-percentY1) * window.innerWidth
+  y1 = rawYMax-y1
+  y2 = rawYMax-y2
+  // percentY2 = (y2-rawYMin)/rangeY
+  // y = (1-percentY2) * window.innerWidth
 }
 
 function handleFinger(finger){
     for(k=1;k<finger.bones.length;k++){
       handleBone(finger.bones[k]);
-      circle(x, y, 50);
+      line(x1,y1,x2,y2);
     }
 
 }
