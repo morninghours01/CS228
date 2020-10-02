f5ff8286f6b7fdb65181c2be7f7a89c84293e1ac
morninghours01 <brycepotts16@gmail.com>
2020-10-02 15:17:39 -0400

    Del04 - step 12 - working

--- irisToNumjs.py
+++ irisToNumjs.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
 
-my_data=pd.read_csv('iris.csv', sep=',',header=1)
-print(my_data)
+my_data=pd.read_csv('iris.csv', sep=',',header=0)
+my_data = my_data.iloc[:,1:-1]
+my_data = np.array2string(np.array(my_data),separator = ',')
+print("var irisData = nj.array(",my_data,");")
