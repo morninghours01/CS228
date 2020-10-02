58841e064429ece9c78aa38c4e1395bcab28f82d
morninghours01 <brycepotts16@gmail.com>
2020-10-02 15:44:15 -0400

    Del04 - step12 - working with class integer

--- irisToNumjs.py
+++ irisToNumjs.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 
 my_data=pd.read_csv('iris.csv', sep=',',header=0)
-my_data = my_data.iloc[:,1:-1]
+
+my_data = my_data.iloc[:,1:]
+my_data = my_data.replace(['Iris-setosa','Iris-versicolor','Iris-virginica'],[0,1,2])
+
 my_data = np.array2string(np.array(my_data),separator = ',')
 print("var irisData = nj.array(",my_data,");")
