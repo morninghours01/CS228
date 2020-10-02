154408bcaa325c946ddfcbb2983e7794ecca6c06
morninghours01 <brycepotts16@gmail.com>
2020-10-02 13:04:51 -0400

    Del04 - Step 9 - working

--- Predict.js
+++ Predict.js
@@ -1,12 +1,19 @@
+let trainingCompleted = false;
 
 function Train(){
-  console.log("I am being Trained")
+  console.log("I am being Trained");
+  trainingCompleted = true;
 }
 
 function Test(){
-  console.log("I am being Tested")
+  console.log("I am being Tested");
 }
 
 function draw(){
   clear();
+  if(!trainingCompleted){
+    Train();
+  }
+
+  Test();
 }
