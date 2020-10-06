8d163c374d72fa59dfe674ba2a4f948c1f753f05
morninghours01 <brycepotts16@gmail.com>
2020-10-05 20:34:05 -0400

    Del05 - step 37 - refactored w reshape tensor fn

--- PredictGestures.js
+++ PredictGestures.js
@@ -4,29 +4,35 @@ let testingSampleIndex = 1;
 
 let trainingCompleted = false;
 
+function reshapeTensor(tensor4d,sample){
+  let tensor3d = tensor4d.pick(null,null,null,sample);
+  let vector = tensor3d.reshape(tensor3d.size);
+  return vector
+}
 
 function Train(){
   console.log("I am being Trained");
   for(i=0; i<train0.shape[3]; i++){
-    let features = train0.pick(null,null,null,i);
-    features = features.reshape(120);
-    //console.log(features.toString());
+    let features = reshapeTensor(train0,i);
+    console.log(features.toString());
     label = 0;
     knnClassifier.addExample(features.tolist(), label);
+
   }
 
   trainingCompleted = true;
 }
 
 function Test(){
-  if(testingSampleIndex==1){
-    console.log("I am being Tested");
-    console.log(test.toString())
+  console.log("I am being Tested");
+
+  let features = reshapeTensor(test,i);
+  knnClassifier.classify(features.tolist(),GotResults);
+
+  testingSampleIndex+=2;
+  if(testingSampleIndex>=numSamples){
+    testingSampleInd
   }
-  testingSampleIndex++;
-
-
-
 }
 
 function draw(){
