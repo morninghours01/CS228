59f87404336b066bf7e51b5f1fcc31ac0135888c
morninghours01 <brycepotts16@gmail.com>
2020-10-05 20:24:01 -0400

    Del05 - step 36 - working

--- PredictGestures.js
+++ PredictGestures.js
@@ -7,19 +7,26 @@ let trainingCompleted = false;
 
 function Train(){
   console.log("I am being Trained");
+  for(i=0; i<train0.shape[3]; i++){
+    let features = train0.pick(null,null,null,i);
+    features = features.reshape(120);
+    //console.log(features.toString());
+    label = 0;
+    knnClassifier.addExample(features.tolist(), label);
+  }
 
-  console.log(train0);
   trainingCompleted = true;
 }
 
 function Test(){
   if(testingSampleIndex==1){
     console.log("I am being Tested");
+    console.log(test.toString())
   }
-  testingSampleIndex++
+  testingSampleIndex++;
 
 
-  console.log(test)
+
 }
 
 function draw(){
